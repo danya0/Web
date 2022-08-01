e16e7e82f40b76f1c0f293bb2a6820cd2d0655fe
Bukharin Maxim <jojojopa@icloud.com>
2022-08-01 10:53:22 +0300

    раздел клиенты и карточка клиента

---
+++ src/components/Clients/ClientCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <ListBlocItem
+    :title="employee.name"
+  >
+    <img
+      class="w-[20px] h-[20px] rounded"
+      :src="employee.img"
+    >
+  </ListBlocItem>
+</template>
+
+<script>
+import ListBlocItem from '@/components/Common/ListBlocItem.vue'
+
+export default {
+  components: {
+    ListBlocItem
+  },
+  props: {
+    employee: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Clients/Clients.vue
+++ src/components/Clients/Clients.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>test</div>
+  <div class="w-full">
+    <ClientCard
+      :employee="employee"
+    />
+  </div>
 </template>
 <script>
+import ClientCard from '@/components/Clients/ClientCard'
+
+export default {
+  components: {
+    ClientCard
+  },
+  data () {
+    return {
+      employee: {
+        name: 'Кто-то там',
+        img: 'https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/reserve/D9xlw7UxTBqQw5sLf8cJ_reef_insp-72.jpg'
+      }
+    }
+  }
+}
 </script>
 <style scoped></style>
